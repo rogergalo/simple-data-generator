16384504731b534b7b710ffba02be0bb7146bd7c
Peter Titov <peter.titov@gmail.com>
2024-11-03 15:10:04 -0500

    Create steps.sh

---
+++ steps.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+# Add enrich-user_agent stuff:
+curl -X POST "http://localhost:30920/enrich-user_agents/_bulk" -H "Content-Type: application/x-ndjson" -u "sdg:changeme" --data-binary @/root/simple-data-generator/enrich-user_agents.ndjson
+# Create user-agents enrich policy
+curl -X PUT "http://localhost:30920/_enrich/policy/user-agents" -H "Content-Type: application/json" -u "sdg:changeme" -d @- << 'EOF'
+{
+  "match": {
+    "indices": "enrich-user_agents",
+    "match_field": "code",
+    "enrich_fields": ["user_agent.os.full"]
+  }
+}
+EOF
+
+curl -X POST "http://localhost:30920/_enrich/policy/user-agents/_execute" -u "sdg:changeme"
