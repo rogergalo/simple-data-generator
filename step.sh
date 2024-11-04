5633cc5cd85d20515b6a6c608d0ae07fac6aa6bd
Peter Titov <peter.titov@gmail.com>
2024-11-04 10:41:03 -0500

    Update step.sh

--- step.sh
+++ step.sh
@@ -20,6 +20,7 @@ source ~/.bashrc
 
 # Add enrich-user_agent stuff:
 curl -X POST "http://localhost:30920/enrich-user_agents/_bulk" -H "Content-Type: application/x-ndjson" -u "sdg:changeme" --data-binary @/root/simple-data-generator/enrich-user_agents.ndjson
+curl -X POST "http://localhost:30920/enrich-windows.sysmon_operational/_bulk" -H "Content-Type: application/x-ndjson" -u "sdg:changeme" --data-binary @/root/simple-data-generator/enrich-windows.sysmon_operational.ndjson
 # Create user-agents enrich policy
 curl -X PUT "http://localhost:30920/_enrich/policy/user-agents" -H "Content-Type: application/json" -u "sdg:changeme" -d @- << 'EOF'
 {
