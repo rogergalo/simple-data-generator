f4ed7862fdbd09f9ad13b57cd4408e45957c5285
Peter Titov <peter.titov@gmail.com>
2024-12-22 14:35:31 -0500

    Create track6.sh

---
+++ track6.sh
@@ -0,0 +1,49 @@
+# Clear the screen
+clear
+
+# Run cmatrix in the background
+cmatrix -b -u 5 &
+
+# Get the process ID of cmatrix to stop it later
+MATRIX_PID=$!
+
+# Wait for 2 seconds to let cmatrix start
+sleep 2
+
+# Hide cursor
+tput civis
+
+# Get terminal dimensions
+rows=$(tput lines)
+cols=$(tput cols)
+
+# Center the message
+message="Loading, please stand by."
+message_length=${#message}
+center_col=$(( (cols - message_length) / 2 ))
+center_row=$(( rows / 2 ))
+
+# Print the message in the center
+tput cup $center_row $center_col
+echo "$message" | cowsay
+
+# Wait for 8 seconds with the message displayed
+sleep 8
+
+# Kill cmatrix process
+kill $MATRIX_PID
+
+# Show cursor again and clear screen
+tput cnorm
+clear
+
+
+
+
+echo "You took the red pill, now we will see how far the rabbit hole goes."
+echo 
+echo
+echo 
+echo
+echo "Starting data ingestion, press CTRL + C to unplug from the Matrix."
+java -jar /root/simple-data-generator/build/libs/simple-data-generator-1.0.0-SNAPSHOT.jar /root/simple-data-generator/secops.yml
