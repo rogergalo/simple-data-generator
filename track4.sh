15463e8173923c544ed6504e230bdae4eba9089f
Peter Titov <peter.titov@gmail.com>
2024-12-22 14:32:14 -0500

    Update track4.sh

--- track4.sh
+++ track4.sh
@@ -1,40 +1,3 @@
-# Create logs-nginx index template
-curl -X PUT "http://localhost:30920/_index_template/nginx" -H "Content-Type: application/x-ndjson" -u "sdg:changeme" -d @/root/simple-data-generator/logs-nginx.json
-curl -X PUT "http://localhost:30920/_index_template/enrich-user_agents" -H "Content-Type: application/x-ndjson" -u "sdg:changeme" -d @/root/simple-data-generator/enrich-user_agents.json
-
-# Load nginx enrich data
-curl -X POST "http://localhost:30920/enrich-nginxv2/_bulk" -H "Content-Type: application/x-ndjson" -u "sdg:changeme" --data-binary @/root/simple-data-generator/enrich-nginxv2.ndjson
-curl -X POST "http://localhost:30920/enrich-user_agents/_bulk" -H "Content-Type: application/x-ndjson" -u "sdg:changeme" --data-binary @/root/simple-data-generator/enrich-user_agents.ndjson
-
-# Create enrich-nginxv2 enrichment 
-curl -X PUT "http://localhost:30920/_enrich/policy/enrich-nginx" -H "Content-Type: application/json" -u "sdg:changeme" -d @- << 'EOF'
-{
-  "match": {
-    "indices": "enrich-nginxv2",
-    "match_field": "code",
-    "enrich_fields": ["http.response.status_code", "event.category", "http.version", "event.kind", "log.file.path", "event.outcome", "event.type", "url.original", "http.request.method"]
-  }
-}
-EOF
-
-# Create enrich-user_agent enrichment 
-curl -X PUT "http://localhost:30920/_enrich/policy/user-agents" -H "Content-Type: application/json" -u "sdg:changeme" -d @- << 'EOF'
-{
-  "match": {
-    "indices": "enrich-user_agents",
-    "match_field": "code",
-    "enrich_fields": ["user_agent.os.full"]
-  }
-}
-EOF
-
-# Execute enrichment policies
-curl -X POST "http://localhost:30920/_enrich/policy/enrich-nginx/_execute" -u "sdg:changeme"
-curl -X POST "http://localhost:30920/_enrich/policy/user-agents/_execute" -u "sdg:changeme"
-
-# Load enrich-nginx pipeline
-curl -X PUT "http://localhost:30920/_ingest/pipeline/enrich-nginx" -H "Content-Type: application/x-ndjson" -u "sdg:changeme" -d @/root/simple-data-generator/enrich-nginx-pipeline.json
-
 # Clear the screen
 clear
 
@@ -82,6 +45,5 @@ echo
 echo
 echo 
 echo
-# cd simple-data-generator && gradle clean; gradle build fatJar
 echo "Starting data ingestion, press CTRL + C to unplug from the Matrix."
 java -jar /root/simple-data-generator/build/libs/simple-data-generator-1.0.0-SNAPSHOT.jar /root/simple-data-generator/secops-ddos.yml
