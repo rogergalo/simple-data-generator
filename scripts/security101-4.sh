4292a855cdbca0bfa24531a1c53b20c2742cf8b2
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2025-03-20 15:02:57 -0400

    Update security101-4.sh

--- scripts/security101-4.sh
+++ scripts/security101-4.sh
@@ -2137,7 +2137,7 @@ clear
 echo "Loading Elastic Rules"
 curl -X PUT "http://localhost:30001/api/detection_engine/rules/prepackaged" -u "sdg:changme"  --header "kbn-xsrf: true" -H "Content-Type: application/json"  -d '{}'
 
-curl -X POST "http://localhost:30001/api/detection_engine/rules/_bulk_create" -u "sdg:changeme" --header "kbn-xsrf: true" -H "Content-Type: application/json" -d -d @/root/simple-data-generator/detection-rules/101-1.json
+curl -X POST "http://localhost:30001/api/detection_engine/rules/_bulk_create" -u "sdg:changeme" --header "kbn-xsrf: true" -H "Content-Type: application/json" -d @/root/simple-data-generator/detection-rules/101-1.json
 
 clear
 
