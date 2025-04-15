3dc17d60a58346b374be0f1ff0658b96ac03f4e3
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2025-04-15 19:37:15 -0400

    Update ingest-to-insights.sh

--- scripts/ingest-to-insights.sh
+++ scripts/ingest-to-insights.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Install git and pull down necessary files
-apt update -y && apt upgrade -y 
-apt install git -y && git clone https://github.com/iamhowardtheduck/ingestworkshop.git
+git clone https://github.com/iamhowardtheduck/ingestworkshop.git
 xz -d /root/ingestworkshop/auth.log.elastic.txt.xz
 mv /root/ingestworkshop/auth.log.elastic.txt /root/ingestworkshop/auth.log.elastic
 cp /root/ingestworkshop/auth.log.elastic /var/log
