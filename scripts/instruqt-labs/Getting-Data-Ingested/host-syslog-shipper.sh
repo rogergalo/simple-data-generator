e863828a513092acc20a99594d714423e37430d2
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2025-05-04 09:12:10 -0400

    Create host-syslog-shipper.sh

---
+++ scripts/instruqt-labs/Getting-Data-Ingested/host-syslog-shipper.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Ensure root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root or with sudo."
+  exit 1
+fi
+
+# Kick off update-etc_hosts.sh
+bash simple-data-generator/scripts/instruqt-labs/Getting-Data-Ingested/update-etc_hosts.sh
+
+# Kick off osquery-setup.sh
+bash simple-data-generator/scripts/instruqt-labs/Getting-Data-Ingested/osquery-setup.sh
+
+# Kick off mysql-docker-deploy.sh
+bash simple-data-generator/scripts/instruqt-labs/Getting-Data-Ingested/mysql-docker-deploy.sh
+
+# Kick off syslog-sending.sh
+bash simple-data-generator/scripts/instruqt-labs/Getting-Data-Ingested/syslog-sending.sh
