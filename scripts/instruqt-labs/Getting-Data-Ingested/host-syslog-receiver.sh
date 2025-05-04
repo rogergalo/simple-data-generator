e4b82e0cbad5693bd9e971af6836535c14b91d04
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2025-05-04 09:13:59 -0400

    Create host-syslog-receiver.sh

---
+++ scripts/instruqt-labs/Getting-Data-Ingested/host-syslog-receiver.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Ensure root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root or with sudo."
+  exit 1
+fi
+
+# Kick off osquery-setup.sh
+bash simple-data-generator/scripts/instruqt-labs/Getting-Data-Ingested/osquery-setup.sh
+
+# Kick off syslog-receiving.sh
+bash simple-data-generator/scripts/instruqt-labs/Getting-Data-Ingested/syslog-receiving.sh
