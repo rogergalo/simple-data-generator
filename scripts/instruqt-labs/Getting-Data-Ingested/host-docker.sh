54443715c5f0798be2bdc9e4e27c926eca89799f
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2025-05-05 16:19:58 -0400

    Update host-docker.sh

--- scripts/instruqt-labs/Getting-Data-Ingested/host-docker.sh
+++ scripts/instruqt-labs/Getting-Data-Ingested/host-docker.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+
+
 # Ensure root
 if [ "$EUID" -ne 0 ]; then
   echo "Please run as root or with sudo."
   exit 1
 fi
+
+# Install nano via snap
+sudo snap install nano --classic
+export USER=$(whoami)
 
 # Kick off update-etc_hosts.sh
 bash simple-data-generator/scripts/instruqt-labs/Getting-Data-Ingested/update-etc_hosts.sh
