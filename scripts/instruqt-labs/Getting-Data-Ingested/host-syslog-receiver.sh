54d162d5e387b417d5539b7e5edc333ba782373b
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2025-05-07 11:24:55 -0400

    Update host-syslog-receiver.sh

--- scripts/instruqt-labs/Getting-Data-Ingested/host-syslog-receiver.sh
+++ scripts/instruqt-labs/Getting-Data-Ingested/host-syslog-receiver.sh
@@ -5,8 +5,11 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
+# Kick off update-etc_hosts.sh
+bash /root/simple-data-generator/scripts/instruqt-labs/Getting-Data-Ingested/update-etc_hosts.sh
+
 # Kick off osquery-setup.sh
-bash simple-data-generator/scripts/instruqt-labs/Getting-Data-Ingested/osquery-setup.sh
+bash /root/simple-data-generator/scripts/instruqt-labs/Getting-Data-Ingested/osquery-setup.sh
 
 # Kick off syslog-receiving.sh
-bash simple-data-generator/scripts/instruqt-labs/Getting-Data-Ingested/syslog-receiving.sh
+bash /root/simple-data-generator/scripts/instruqt-labs/Getting-Data-Ingested/syslog-receiving.sh
