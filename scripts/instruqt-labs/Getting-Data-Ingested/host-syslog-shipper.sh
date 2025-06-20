4a21e1e56cccd2712ec4c041b6e21a9a59248b19
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2025-06-20 09:51:42 -0400

    Update host-syslog-shipper.sh

--- scripts/instruqt-labs/Getting-Data-Ingested/host-syslog-shipper.sh
+++ scripts/instruqt-labs/Getting-Data-Ingested/host-syslog-shipper.sh
@@ -5,14 +5,11 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-# Kick off update-etc_hosts.sh
-bash simple-data-generator/scripts/instruqt-labs/Getting-Data-Ingested/update-etc_hosts.sh
-
 # Kick off osquery-setup.sh
 bash simple-data-generator/scripts/instruqt-labs/Getting-Data-Ingested/osquery-setup.sh
 
-# Kick off mysql-docker-deploy.sh
-bash simple-data-generator/scripts/instruqt-labs/Getting-Data-Ingested/mysql-docker-deploy.sh
-
 # Kick off syslog-sending.sh
 bash simple-data-generator/scripts/instruqt-labs/Getting-Data-Ingested/syslog-sending.sh
+
+# Kick off install-fim-chaos.sh
+bash simple-data-generator/scripts/instruqt-labs/Getting-Data-Ingested/install-fim-chaos.sh
