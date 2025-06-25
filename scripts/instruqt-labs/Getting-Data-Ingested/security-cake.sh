8d1d05f163d4649bcb0704617c0627bd3d3588f4
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2025-06-24 22:22:59 -0400

    Update security-cake.sh

--- scripts/instruqt-labs/Getting-Data-Ingested/security-cake.sh
+++ scripts/instruqt-labs/Getting-Data-Ingested/security-cake.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+# Ensure root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root or with sudo."
+  exit 1
+fi
 
+# Define the URL and filename
+URL="https://downloads.plex.tv/plex-media-server-new/1.41.8.9834-071366d65/debian/plexmediaserver_1.41.8.9834-071366d65_amd64.deb"
+FILE="plexmediaserver_1.41.8.9834-071366d65_amd64.deb"
+
+# Download the file
+wget "$URL" -O "$FILE"
+
+# Install the package
+sudo dpkg -i "$FILE"
