0f2089ec8321a53b3b069d6388599abf4e09482c
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2025-07-04 08:46:29 -0400

    Update ca.sh

--- scripts/instruqt-labs/Getting-Data-Ingested/ca.sh
+++ scripts/instruqt-labs/Getting-Data-Ingested/ca.sh
@@ -4,8 +4,6 @@
 SRC_CERT="/etc/ssl/certs/nginx-selfsined.crt"
 DEST_CERT="/usr/local/share/ca-certificates/ca.crt"
 DEST_HOST1="docker"
-DEST_HOST2="syslog-shipper"
-DEST_HOST3="syslog-aggregator"
 
 # HOST 1
 
@@ -23,43 +21,5 @@ if [ $? -eq 0 ]; then
     echo "Certificate installed and CA store updated on $DEST_HOST1."
 else
     echo "Failed to update CA store on $DEST_HOST1."
-    exit 2
-fi
-
-# HOST2
-
-echo "Copying $SRC_CERT to $DEST_HOST2:$DEST_CERT ..."
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "$SRC_CERT" "$DEST_HOST2:/tmp/ca.crt"
-if [ $? -ne 0 ]; then
-    echo "SCP failed! Exiting."
-    exit 1
-fi
-
-echo "Moving cert to $DEST_CERT and updating CA store on $DEST_HOST2 ..."
-ssh -o StrictHostKeyChecking=accept-new "$DEST_HOST2" "sudo mv /tmp/ca.crt $DEST_CERT && sudo update-ca-certificates"
-
-if [ $? -eq 0 ]; then
-    echo "Certificate installed and CA store updated on $DEST_HOST2."
-else
-    echo "Failed to update CA store on $DEST_HOST2."
-    exit 2
-fi
-
-# HOST 3
-
-echo "Copying $SRC_CERT to $DEST_HOST3:$DEST_CERT ..."
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "$SRC_CERT" "$DEST_HOST3:/tmp/ca.crt"
-if [ $? -ne 0 ]; then
-    echo "SCP failed! Exiting."
-    exit 1
-fi
-
-echo "Moving cert to $DEST_CERT and updating CA store on $DEST_HOST3 ..."
-ssh -o StrictHostKeyChecking=accept-new "$DEST_HOST3" "sudo mv /tmp/ca.crt $DEST_CERT && sudo update-ca-certificates"
-
-if [ $? -eq 0 ]; then
-    echo "Certificate installed and CA store updated on $DEST_HOST3."
-else
-    echo "Failed to update CA store on $DEST_HOST3."
     exit 2
 fi
